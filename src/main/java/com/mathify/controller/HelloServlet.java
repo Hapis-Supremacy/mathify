c9e7b12d9ecec852162650a8b28319d55f844475
Gimerbone <gimerbone@gmail.com>
2026-05-19 20:34:47 +0700

    Moved Hello Servlet to controller package

---
+++ src/main/java/com/mathify/controller/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.mathify.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
+        response.setContentType("text/html");
+        request.getRequestDispatcher("/index.jsp")
+                .forward(request, response);
+    }
+}
